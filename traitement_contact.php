b31199d3bf65743f1cbfc7dd4a2218b411087600
Aphios <sphblanchard@gmail.com>
2021-05-11 10:06:23 +0200

    modified contact

--- traitement_contact.php
+++ traitement_contact.php
@@ -21,7 +21,7 @@
 
 	// Connexion BDD
 	try{
-    	$bdd = new PDO('mysql:host=localhost;dbname=aphios_website_contacts;charset=utf8', 'root', 'root', 
+    	$bdd = new PDO('mysql:host=localhost;dbname=aphios_website_contacts;charset=utf8', '****', '****', 
         	array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	}catch(Exception $e){
     	die('Erreur : ' . $e->getMessage());
